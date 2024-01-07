c4e4e0fa3ed1e9baa93e2586cde0008a1f47e8d1
shivang-16 <shivangyadav121@gmail.com>
2024-01-07 22:10:49 +0530

    update

--- dist/routers/resultRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getUserResult, setResult } from "../controllers/resultController.js";
-import { isAuthenticated } from "../middleware/auth.js";
-const router = express.Router();
-router.post("/save", isAuthenticated, setResult);
-router.get("/", isAuthenticated, getUserResult);
-export default router;
